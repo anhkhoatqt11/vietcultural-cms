55129f42dc690a21374ff24d96f6d04880a5871f
theunderdawg2005 <lqd.tik22.dinhcongthao@gmail.com>
2025-04-25 10:59:18 +0700

    fix: add badge, process, region id to achievement

--- src/collections/Achievments.ts
+++ src/collections/Achievments.ts
@@ -5,14 +5,27 @@ export const Achievements: CollectionConfig = {
   admin: { useAsTitle: 'name' },
   fields: [
     { name: 'userId', type: 'relationship', relationTo: 'user', required: true },
+    { name: 'regionId', type: 'relationship', relationTo: 'regions', required: true },
     { name: 'name', type: 'text', required: true },
     { name: 'description', type: 'text', required: true },
-    { name: 'stars', type: 'number', required: true },
+    { name: 'history', type: 'checkbox', defaultValue: false },
+    {
+      name: 'intangible_heritage',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    {
+      name: 'tangible_heritage',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    { name: 'stars', type: 'number', defaultValue: 0 },
+    { name: 'badge', type: 'upload', relationTo: 'media' },
   ],
-  access:{
+  access: {
     read: () => true,
     create: () => true,
     update: () => true,
     delete: () => true,
-  }
+  },
 }
